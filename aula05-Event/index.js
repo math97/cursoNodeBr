ba74d4d92830d7694a058f62f3619f72cec83240
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2019-02-28 14:12:09 -0300

    Fim de promisse,events

---
+++ aula05-Event/index.js
@@ -0,0 +1,20 @@
+const EventEmitter = require('events')
+
+class MeuEmissor extends EventEmitter{
+
+
+}
+const meuEmissor = new MeuEmissor();
+const nomeDoEvento = 'usuario:click';
+
+meuEmissor.on(nomeDoEvento,function(click){
+    console.log('um usuario clicou',click)
+})
+
+meuEmissor.emit(nomeDoEvento,'na barra');
+meuEmissor.emit(nomeDoEvento,'no ok');
+
+let count = 0;
+setInterval(function(){
+    meuEmissor.emit(nomeDoEvento,'no ok'+(count++))
+},1000)
